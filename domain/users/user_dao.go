dcde0f3c925759c7bc8573fde1f39c487a8a8801
Bobby Gifford <bobby077joe@gmail.com>
2021-03-09 11:40:13 -0800

    Implemented Save() to MySql

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/BobbyGifford/go_bookstore_users_api/utils/errors"
 	"github.com/bobbygifford/go_bookstore_users_api/datasources/mysql/users_db"
-	"github.com/bobbygifford/go_bookstore_users_api/utils/date"
+	"github.com/bobbygifford/go_bookstore_users_api/utils/date_utils"
+	"strings"
 )
 
 // Data access object
@@ -14,7 +15,12 @@ var (
 	usersDB = make(map[int64]*User)
 )
 
-func (user *User) Get () *errors.RestErr {
+const (
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	indexUniqueEmail = "users.users_email_uindex"
+)
+
+func (user *User) Get() *errors.RestErr {
 	if err := users_db.Client.Ping(); err != nil {
 		panic(err)
 	}
@@ -35,17 +41,32 @@ func (user *User) Get () *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	currentUser := usersDB[user.Id]
-	if currentUser!= nil {
-		if user.Email == currentUser.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("user %s already exists", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	user.DateCreated = date.GetNowString()
+	defer stmt.Close()
 
-	// Save if user is not in DB
-	usersDB[user.Id] = user
+	user.DateCreated = date_utils.GetNowString()
+
+	insertResult, insertErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if insertErr != nil {
+		if strings.Contains(insertErr.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user %s", insertErr.Error()))
+	}
+
+	// Same as above (Worse performance and no query validation)
+	// result, err := users_db.Client.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	userId, idErr := insertResult.LastInsertId()
+	if idErr != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user %s", idErr.Error()))
+	}
+
+	user.Id = userId
+
 	return nil
 }
